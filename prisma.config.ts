d956bc1a95076c7982cd177c1ae30fcde8aefa45
Nguyen Huy Hung <nguyen.huy.hung-c@sun-asterisk.com>
2026-06-30 15:48:04 +0700

    update config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ loadEnv()
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL ?? '',
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? '',
   },
 })
